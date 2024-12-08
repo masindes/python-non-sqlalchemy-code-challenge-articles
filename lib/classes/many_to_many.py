bbc9a438515fb569bc228366b9d44c028f4d973b
masinde sylvester <masinde.sylvester@yahoo.com>
2024-12-08 11:12:11 +0300

    initial commit

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -73,15 +73,48 @@ class Author:
         # pass
 
     def topic_areas(self):
-        pass
+        top_areas = list({Magazine.category for magazine in self.magazine()})
+        if top_areas:
+            return top_areas
+        else:
+            return []
+        # pass
 
 class Magazine:
     def __init__(self, name, category):
         self.name = name
         self.category = category
+    @property
+    def name(self):
+        return self._name
 
+    @name.setter
+    def name(self, new_name):
+        if isinstance(new_name, str):
+            if 2 <= len(new_name) <= 16:
+                self._name = new_name
+            else: 
+                ValueError("Name must be between 2 and 16 characters")
+        else:
+            TypeError("Name must be a string") 
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, new_category):
+        if isinstance(new_category, str):
+            if len(new_category):
+                self._category = new_category
+            else:
+                ValueError("Category must be longer than 0 characters")
+        else:
+            TypeError("Category must be a string") 
+    
     def articles(self):
-        pass
+        return [article for article in Article.all if self == article.magazine]
+        # pass
 
     def contributors(self):
         pass
